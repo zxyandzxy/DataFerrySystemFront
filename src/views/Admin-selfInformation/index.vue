00e83e38be4cc1d3c0691ffd44bc3343e95b3717
Zhou Junpeng <2427098840@qq.com>
2024-08-29 18:27:31 +0800

    获取远端main更新

--- src/views/Admin-selfInformation/index.vue
+++ src/views/Admin-selfInformation/index.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="app-container">
-    <div class="app-container-inner">
-      我是个人信息
-    </div>
+    <div class="app-container-inner"> 我是个人信息 </div>
   </div>
 </template>
 <script setup lang="ts"></script>
