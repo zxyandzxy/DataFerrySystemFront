b48ad2ade1149ae55d37e885031bd14a85ddb811
Zhou Junpeng <2427098840@qq.com>
2024-09-09 21:08:16 +0800

    9.9

--- src/api/admin-teacher.ts
+++ src/api/admin-teacher.ts
@@ -3,36 +3,43 @@ import { Admin } from '../admin-interface/teacher'
 
 // 获取所有管理员信息
 export function fetchAdmins(): Promise<Admin[]> {
-  try {
-    return service.get<Admin[]>('/admin/adminlist').then((response) => response.data)
-  } catch (error) {
-    throw new Error(`Failed to fetch admins ${error.message}`)
-  }
+  return service.get<Admin[]>('/manager/get_admin_list').then((response) => response.data)
 }
 
-// 添加管理员信息
-export function addAdmin(admin: Admin): Promise<Admin> {
-  try {
-    return service.post('/admin/addAdmin', admin).then((response) => response.data)
-  } catch (error) {
-    throw new Error(`Failed to add admin: ${error.message}`)
-  }
+// 添加管理员
+export function addAdmin(adminAccount: string, adminName: string): Promise<{ password: string }> {
+  return service
+    .post('/manager/add_admin', { adminAccount, adminName })
+    .then((response) => response.data)
+}
+
+// 重置管理员密码
+export function resetManagerPassword(adminAccount: string): Promise<{ password: string }> {
+  return service
+    .put('/manager/reset_manager_password', null, {
+      params: {
+        adminAccount,
+      },
+    })
+    .then((response) => response.data)
 }
 
 // 删除管理员信息
-export function removeAdmin(adminId: string): Promise<void> {
-  try {
-    return service.delete(`/admins/${adminId}`)
-  } catch (error) {
-    throw new Error(`Failed to remove admin: ${error.message}`)
-  }
+export function removeAdmin(adminAccount: string): Promise<void> {
+  return service.delete(`/manager/delete_admin?adminAccount=${adminAccount}`)
 }
 
 // 更新管理员信息
 export function updateAdmin(adminId: string, admin: Admin): Promise<Admin> {
-  try {
-    return service.put(`admints/${adminId}`, admin).then((response) => response.data)
-  } catch (error) {
-    throw new Error(`Failed to update admin: ${error.message}`)
-  }
+  return service.put(`admints/${adminId}`, admin).then((response) => response.data)
+}
+
+// 登录API
+export const adminLoginAPI = (data) => {
+  return service.post('/api/admin/login', data)
+}
+
+// 获取管理员信息API
+export const adminGetInfoAPI = (adminId) => {
+  return service.get(`/api/admin/info/${adminId}`)
 }
